2c72c22f5b9157c11aa5ba14a0421b1adba51333
Eduardo Nunes <eduardo.nunes@ifactory.com.br>
2018-06-19 16:15:48 -0300

    added ADS form IMA

--- mobile/src/main/java/com/nunes/eduardo/videoapp/ExoPlayerIOFragment.kt
+++ mobile/src/main/java/com/nunes/eduardo/videoapp/ExoPlayerIOFragment.kt
@@ -9,25 +9,31 @@ import android.view.View
 import android.view.ViewGroup
 import com.google.android.exoplayer2.ExoPlayerFactory
 import com.google.android.exoplayer2.SimpleExoPlayer
+import com.google.android.exoplayer2.ext.ima.ImaAdsLoader
 import com.google.android.exoplayer2.source.ExtractorMediaSource
+import com.google.android.exoplayer2.source.ads.AdsMediaSource
 import com.google.android.exoplayer2.trackselection.DefaultTrackSelector
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.fragment_exo_player_io.*
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.Reader
 
 private const val ARG_PARAM1 = "source"
 private const val ARG_PARAM2 = "title"
 
 class ExoPlayerIOFragment : Fragment() {
     private var player: SimpleExoPlayer? = null
-    private var videoSource: String? = null
+    private var adsLoader: ImaAdsLoader? = null
+    private var videoSrc: String? = null
     private var videoTitle: String? = null
     private var listener: OnFragmentInteractionListener? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
-            videoSource = it.getString(ARG_PARAM1)
+            videoSrc = it.getString(ARG_PARAM1)
             videoTitle = it.getString(ARG_PARAM2)
         }
     }
@@ -41,22 +47,35 @@ class ExoPlayerIOFragment : Fragment() {
         super.onStart()
         player = ExoPlayerFactory.newSimpleInstance(activity, DefaultTrackSelector())
 
+        val reader: Reader = BufferedReader( InputStreamReader(resources.openRawResource(R.raw.tag), "UTF8"))
+
+        reader.use {
+            adsLoader = ImaAdsLoader(activity, Uri.parse(Uri.decode(it.readText())))
+        }
+
         exoPlayerView.player = player
+        exoPlayerView.useController = true
+        exoPlayerView.controllerHideOnTouch = true
+        exoPlayerView.controllerAutoShow = true
 
         val dataSourceFactory = DefaultDataSourceFactory(activity,
                 Util.getUserAgent(activity, "exo-demo"))
 
         var mediaSource: ExtractorMediaSource? = null
-
-        videoSource?.let {
+        videoSrc?.let {
             mediaSource = ExtractorMediaSource
                     .Factory(dataSourceFactory)
                     .createMediaSource(Uri.parse(Uri.decode(it)))
         }
 
-        mediaSource?.let {
-            player?.prepare(it)
-        }
+        val adsMediaSource = AdsMediaSource(mediaSource, dataSourceFactory, adsLoader,
+                exoPlayerView.overlayFrameLayout)
+
+        player?.prepare(adsMediaSource)
+
+//        mediaSource?.let {
+//            player?.prepare(it)
+//        }
 
         player?.playWhenReady = true
     }
